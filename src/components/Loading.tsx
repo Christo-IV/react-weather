b9244a837e4ac3f99e9dfe961a7c105fa8395334
christo.vahtramae <christo.vahtramae@khk.ee>
2021-12-31 12:27:15 +0200

    Fixed CSS var issue in loading comp

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,16 +1,16 @@
+import React, { CSSProperties } from "react";
 import "./CSS/Loading.css";
 
 const Loading = () => {
+  interface CSSvar extends CSSProperties {
+    "--i": number;
+  }
+
   return (
     <div className="loader-wrapper">
       {["L", "o", "a", "d", "i", "n", "g", ".", ".", "."].map(
         (character, index) => {
-          let style = { "--i": index };
-
-          // I don't know what this error is exactly,
-          // but it doesn't seem to interfere ¯\_(ツ)_/¯
-
-          return <span style={style}>{character}</span>;
+          return <span style={{ "--i": index } as CSSvar}>{character}</span>;
         }
       )}
     </div>
